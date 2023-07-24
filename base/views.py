3eaafaea29ecba78d1cc07ea73b5249a7fdb767d
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-07-24 09:08:18 +0300

    configure urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'home.html', context)
+
+def rooms(request):
+    context = {}
+    return render(request, 'rooms.html', context)
