e0e732fa7347e2ffc43cff827cefc13869045763
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-07-25 16:48:10 +0300

    user login functionality

--- base/views.py
+++ base/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
+from django.contrib.auth.models import User
 from .forms import RoomForm
 from django.db.models import Q
 
 # Create your views here.
+
+def login_user(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User Does not Exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password Does not Exist')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
