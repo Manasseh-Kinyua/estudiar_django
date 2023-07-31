e3e26f097e20997365525958c10e677f1c2fabb3
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-07-31 09:59:29 +0300

    edit profile functionality

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Room, Topic, Message
 from django.contrib.auth.models import User
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 
@@ -208,5 +208,17 @@ def delete_message(request, pk):
 
 @login_required(login_url='login')
 def update_user(request):
-    context = {}
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {
+        "user": user,
+        "form": form,
+    }
     return render(request, 'base/update_user.html', context)
